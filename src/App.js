3e6452024cb521b531101fffb7356c66dc93aa3a
Veronica <svcl_svcl@hotmail.com>
2020-05-12 05:42:12 -0600

    CRUD unidad organizacional

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import EmpresaComponent from './Components/Empresa/Empresa';
 import EmpresaDetalleComponent from './Components/Empresa/EmpresaDetalle';
 import HomeComponent from './Components/Home/Home';
 
+import UnidadOrganizacionalComponent from './Components/UnidadOrganizacional/UnidadOrganizacional';
+import UnidadOrganizacionalDetalle from './Components/UnidadOrganizacional/UnidadOrganizacionalDetalle';
+
 function App() {
   return (
     <div className="App">
@@ -15,6 +18,13 @@ function App() {
           <Route exact path="/" component={HomeComponent}></Route>
           <Route exact path="/empresa" component={EmpresaComponent}></Route>
           <Route path="/empresa/:id" component={EmpresaDetalleComponent} ></Route>
+
+          <Route exact path="/departamentos" component={UnidadOrganizacionalComponent} />
+          <Route path="/departamentos/crear" component={UnidadOrganizacionalDetalle} />
+          <Route path="/departamentos/editar/:id"
+          render={ (props) =><UnidadOrganizacionalDetalle {...props} editar={true}
+          />
+          }/>
         </div>
       </Router>
     </div>
