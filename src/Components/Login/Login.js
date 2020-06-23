173803a3ba5cae3f828604cade67bd974125743b
salvador23 <salvadoramelia2@gmail.com>
2020-06-23 01:57:00 -0600

    user and rol view, login

---
+++ src/Components/Login/Login.js
@@ -0,0 +1,132 @@
+import React, { useState } from "react";
+import { Button, FormGroup, FormControl } from "react-bootstrap";
+import Swal from 'sweetalert2';
+import Alert from 'react-bootstrap/Alert';
+import { Formik, Form, Field, ErrorMessage } from 'formik'
+import  { Redirect } from 'react-router-dom'
+
+import "../../Container/Login.css";
+import LoginService from '../../Service/Login/LoginService';
+let contadorFallida = 0;
+let usernameAnterior='';
+export default class Login extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			username: '',
+			password: ''
+		};
+
+		this.onSubmit = this.onSubmit.bind(this);
+		this.validate = this.validate.bind(this);
+	}
+	componentDidMount(){
+		if(LoginService.isAuthenticated()){
+			console.log("ya esta")
+			this.props.history.push('/home');
+		}
+	}
+
+	validate(values){
+		let errors ={}
+		if(!values.username) errors.username='Ingrese nombre de usuario';
+		if(!values.password) errors.password ='Ingrese password';
+		return errors;
+	}
+
+	async onSubmit(values){
+		const usuario = {
+			username: values.username,
+			password: values.password,
+		}
+		this.setState({username:values.username});
+		LoginService.login(usuario).then(res=>{
+			console.log(res);
+			let objeto_usuario_payload = LoginService.obtenerDatosToken(res.data.access_token);
+			LoginService.guardarUsuario(res.data.access_token);
+			LoginService.guardarToken(res.data.access_token);
+			let username = LoginService.obtenerUsuario();
+			console.log(username)
+			Swal.fire({icon: 'success',title: 'Sesión Iniciada',text: `Has iniciado sesión con éxito ${username.username}!`
+		})
+			contadorFallida = 0;
+			console.log(contadorFallida);
+			window.location.reload(false);
+			this.props.history.push('/home');
+		}).catch( (err)=> {
+			console.log(err.response)
+			if(err.response.status == 400 || err.response.status==401){
+				if (!(usernameAnterior===this.state.username)) {
+					console.log("entra al ser usuario diferente")
+					contadorFallida=0;
+					usernameAnterior = this.state.username;
+				}
+				contadorFallida=contadorFallida+1;
+				if(err.response.data.error_description === "User is disabled"){
+					Swal.fire(
+					'Estado de cuenta',
+					'Su cuenta de usuario ha sido bloqueada',
+					'info'
+					)
+					contadorFallida=0;
+				}else{
+					Swal.fire(
+					'Algo ha salido mal',
+					'username o password no son las correctas',
+					'error'
+					)
+				}
+				console.log("Contador fallida:"+contadorFallida);
+				
+			}
+			if (contadorFallida===3) {
+				LoginService.usuarioBloquear(this.state.username).then( (res)=>{
+                Swal.fire(
+					'Estado de cuenta',
+					res.data.mensaje,
+					'info'
+					)
+        		} );
+        		contadorFallida=0;
+			}
+		} );
+	}
+	render(){
+		let {username,password} = this.state;
+		return (
+			<div className="Login">
+			<h4>Iniciar Sesión</h4>
+			<Formik
+			initialValues={{ username,password}}
+			validateOnChange={false}
+			validateOnBlur={false}
+			validate={this.validate}
+			enableReinitialize={true}
+			onSubmit={this.onSubmit}
+			>
+			{
+				<Form> 
+				<ErrorMessage name="username" component="div"
+				className="alert alert-warning" />
+				<fieldset className="form-group">
+				<label htmlFor="">Username:</label>
+				<Field className="form-control" type="text" placeholder="Nombre" name="username" />
+				</fieldset>
+
+				<ErrorMessage name="password" component="div"
+				className="alert alert-warning" />
+				<fieldset className="form-group">
+				<label htmlFor="">Password:</label>
+				<Field className="form-control" type="password" placeholder="Password" name="password" />
+				</fieldset>
+
+				<button className="btn btn-success" type="submit">Sign In</button>
+				</Form>
+			}
+			</Formik>
+			</div>
+			);
+
+		}
+	}
---
+++ src/Service/Login/LoginService.js
@@ -0,0 +1,146 @@
+import axios from 'axios';
+import Swal from 'sweetalert2';
+import { BASE_API_URL } from '../../utilities/constants'
+
+const credenciales = btoa('reactapp'+':'+'12345');
+const headers = {
+	'Content-Type': 'application/x-www-form-urlencoded',
+	'Authorization': 'Basic '+credenciales
+}
+let _usuario = {
+	id:'',
+	username:'',
+	email:'',
+	permisos:[]
+}
+
+let _token=''
+class LoginService {
+
+	login(usuario){
+		console.log(usuario.username)
+		console.log(usuario.password)
+		let params = new URLSearchParams();
+		params.set('grant_type','password');
+		params.set('username',usuario.username);
+		params.set('password',usuario.password);
+		return axios.post(`${BASE_API_URL}/oauth/token`,params.toString(),{headers: headers});
+	}
+
+	obtenerDatosToken(accessToken){
+		return accessToken != null ? JSON.parse(atob(accessToken.split(".")[1])) : "";
+	}
+
+	guardarUsuario(accessToken){
+		let payload = this.obtenerDatosToken(accessToken);
+		_usuario.id = payload.id
+		_usuario.username = payload.username;
+		_usuario.email = payload.email;
+		_usuario.permisos = payload.authorities;
+		sessionStorage.setItem('usuario',JSON.stringify(_usuario));
+	}
+
+	guardarToken(accessToken){
+		_token = accessToken
+		sessionStorage.setItem('token',accessToken);
+	}
+
+	
+
+	obtenerUsuario(){
+		if(_usuario.username !=''){
+			console.log("hay datos en usuario")
+			console.log(_usuario.username)
+			return _usuario;
+		}else if(_usuario.username == '' && sessionStorage.getItem('usuario') !=null){
+			console.log("no hay datos en _usuario y busca sessionStorage")
+			let payload=JSON.parse(sessionStorage.getItem('usuario'));
+			console.log(payload)
+			_usuario.id = payload.id
+			_usuario.username = payload.username;
+			_usuario.email = payload.email;
+			_usuario.permisos = payload.permisos;
+			console.log(_usuario)
+			return _usuario
+		}
+		console.log("retorna null")
+		return null;
+	}
+
+	obtenerToken(){
+		if(_token !=''){
+			console.log("hay token")
+			return _token;
+		}else if(_token == '' && sessionStorage.getItem('token') !=null){
+			console.log("no hay busca en storage")
+			_token = sessionStorage.getItem('token');
+			return _token
+		}
+
+		return null;
+	}
+
+	isAuthenticated(){
+		let payload = this.obtenerDatosToken(this.obtenerToken());
+		if(payload !=null && payload.username && payload.username.length > 0){
+			console.log("retorna true")
+			return true;
+		}
+
+		console.log("retorna false")
+		return false;
+	}
+
+	logout(){
+		_token = null;
+		_usuario = null
+		sessionStorage.clear();
+	}
+
+	agregarAuthorizationHeader(){
+		let headers = {
+			'Content-Type': 'application/json'
+		}
+
+		//console.log(headers);
+		let token = this.obtenerToken();
+		if(token != null){
+			let headers = {
+			'Content-Type': 'application/json',
+			'Authorization':'Bearer '+token,
+		}
+			//return headers["Authorization"] = 'Bearer ' + token;
+			return headers;
+		}
+		return headers;
+	}
+
+	isNoAutorizado(e){
+		if(e.status == 401){
+			if(this.isAuthenticated()){
+				this.logout();
+			}
+		}
+		if(e.status == 401 || e.status == 403){
+
+			return true;
+		}
+
+		return false;
+	}
+
+	hasPermiso(permiso){
+		let usuario = this.obtenerUsuario();
+		if(usuario.permisos.includes(permiso)){
+			return true;
+		}
+		return false;
+	}
+
+	usuarioBloquear(username){
+		return axios.get(`${BASE_API_URL}/api/planilla/user/bloqueado/${username}`,{headers: this.agregarAuthorizationHeader()});
+	}
+
+}
+
+export default new LoginService();
