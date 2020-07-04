6d8d755052c1a3d78346b67d747f680f3edb86f7
edwin <edwinenriquedimas@gmail.com>
2020-07-04 15:05:56 -0600

    Cambio de globals para produccion.

--- src/utilities/constants.js
+++ src/utilities/constants.js
@@ -1,6 +1,7 @@
 
-export const BASE_API_URL = 'http://10.10.10.115:8000';
+//export const BASE_API_URL = 'http://10.10.10.115:8000';
 
-//export const BASE_API_URL = 'https://bad115-grupo7.herokuapp.com/';
+export const BASE_API_URL = 'https://bad115-grupo7.herokuapp.com';
 //export const BASE_API_URL = 'http://localhost:8000'
 export const BASE_API_PLANILLA = 'api/planilla';
+
